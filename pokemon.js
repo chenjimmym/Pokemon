168e2e3ff16f6e4dfcade3df8ce0c6e4fd69e258
Jimmy <chen.jimmym@gmail.com>
2017-09-29 13:47:46 -0700

    done, design needed

--- pokemon.js
+++ pokemon.js
@@ -8,16 +8,24 @@ $(document).ready(function(){
         var id = $(this).attr('id');
         // alert(id);
         $.get(`http://pokeapi.co/api/v1/pokemon/${id}/`, function(result) {
-            console.log(result);
-            alert(result.name);
+            // console.log(result);
+            var type = '';
+            for(var i = 0; i < result.types.length; i++){
+                type = type + '<li>' + result.types[i].name + '</li>';
+                // console.log('>>>>>>>>' + type);
+                // alert(result.types[i].name);    
+            }
             $('#stat').html(`
                 <h2>${result.name}</2>
                 <img src="https://pokeapi.co/media/img/${id}.png" alt="number ${id}" width="60">
                 <h3>Type</h3>
                 <ul>
-                    <li>${result.name}</li>
-                    <li>${result.name}</li>
+                    ${type}
                 </ul>
+                <h3>Height</h3>
+                <p>${result.height}</p>
+                <h3>Wight</h3>
+                <p>${result.weight}</p>
             `);
         }, "jsonp");      
     });
